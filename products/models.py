67ba64712497f90add8090e2eb12e45bd6d0a508
jacopo <71777025+JacopoSimonini@users.noreply.github.com>
2023-06-13 17:48:18 +0200

    Update models.py

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_photos', null=True, blank=True)
+    image = models.ImageField(upload_to='product_photos')
 
 
     def __str__(self):
